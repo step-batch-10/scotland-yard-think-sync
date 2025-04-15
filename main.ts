7773b9765a2704461cc269cd210d61f2207a3c59
Akash <deymrakash99@gmail.com>
2025-04-15 17:03:22 +0530

    added github action

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-import { Hono } from 'hono';
-import { serveStatic } from 'hono/deno';
+import { Hono } from "hono";
+import { serveStatic } from "hono/deno";
 
 const app = new Hono();
-app.get(serveStatic({ root: './public' }));
+app.get(serveStatic({ root: "./public" }));
 
 Deno.serve(app.fetch);
