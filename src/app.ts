59b54758bbc2cab2417bb77b32e22bb838ebfcb7
PruthviLahare <142437821+PruthviLahare@users.noreply.github.com>
2025-04-16 12:11:01 +0530

    implemented player authenticatication

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,24 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { getCookie } from "hono/cookie";
 import { serveStatic } from "hono/deno";
+import { MiddlewareHandler } from "hono/types";
+
+const ensureAthenticated: MiddlewareHandler = async (context, next) => {
+  const cookie = getCookie(context, "playerId");
+  if (cookie) {
+    return await next();
+  }
+
+  return context.redirect("/login", 303);
+};
 
 export const createApp = (): Hono => {
   const app = new Hono();
+
+  app.use(logger());
+  app.use(ensureAthenticated);
+  app.get("/login", serveStatic({ path: "./public/html/login.html" }));
   app.get(serveStatic({ root: "./public" }));
 
   return app;
--- test/app_test.ts
+++ test/app_test.ts
@@ -1,5 +1,13 @@
-import { assert } from "assert";
+import { createApp } from "../src/app.ts";
+import { describe, it } from "testing";
+import { assertEquals } from "jsr:@std/assert";
 
-Deno.test("first test case", () => {
-  assert(true);
+describe("ensure Authentication", () => {
+  it("should get redirected to the login page because player is not logined", async () => {
+    const app = createApp();
+    const res = await app.request("/");
+
+    assertEquals(res.status, 303);
+    assertEquals(res.headers.get("location"), "/login");
+  });
 });
