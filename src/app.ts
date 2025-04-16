c44fb1bae96196d80c22a7a2973ddcfe0af3382b
akshay-011 <102289107+akshay-011@users.noreply.github.com>
2025-04-16 11:27:06 +0530

    Adds  login handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/deno";
+import { Handler } from "hono/types";
+
+const loginHandler: Handler = (context) => {
+  return context;
+};
 
 export const createApp = (): Hono => {
   const app = new Hono();
   app.get(serveStatic({ root: "./public" }));
+
+  app.post("/login", loginHandler);
 
   return app;
 };
