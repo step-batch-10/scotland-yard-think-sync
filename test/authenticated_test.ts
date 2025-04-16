79a450f3ca6a177edc53e3ef730321068a04af4b
PruthviLahare <142437821+PruthviLahare@users.noreply.github.com>
2025-04-16 16:31:16 +0530

    #4 | Fixes lint bug

--- test/authenticated_test.ts
+++ test/authenticated_test.ts
@@ -1,6 +1,6 @@
 import { createApp } from "../src/app.ts";
 import { describe, it } from "testing";
-import { assert, assertEquals, assertFalse } from "jsr:@std/assert";
+import { assert, assertEquals } from "jsr:@std/assert";
 import { PlayerRegistry } from "../src/models/players.ts";
 import { Rooms } from "../src/models/rooms.ts";
 import { Bindings } from "../src/models/types.ts";
