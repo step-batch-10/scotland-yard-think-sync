995e4d2354b06460059618911a5094cb7212d655
PruthviLahare <142437821+PruthviLahare@users.noreply.github.com>
2025-04-17 18:36:39 +0530

    [#7] | Akash/Pruthvi | Displayed tickets data in the frontend

--- public/script/game.js
+++ public/script/game.js
@@ -1,9 +1,9 @@
 const cloneTemplate = (targetId) => {
   const template = document.querySelector(targetId);
-  return template.content.firstElementChild.cloneNode(true);
+  return template.content.cloneNode(true);
 };
 
-const makeRow = (data) => {
+const makeRoleRow = (data) => {
   const trElement = cloneTemplate("#role-row");
   const rows = trElement.querySelectorAll("td");
 
@@ -14,33 +14,50 @@ const makeRow = (data) => {
   return trElement;
 };
 
-const renderRoles = (rolesObject) => {
+const makeTicketRow = (data) => {
+  const trElement = cloneTemplate("#ticket-row");
+  const rows = trElement.querySelectorAll("td");
+
+  rows[0].textContent = data[0];
+  rows[1].textContent = data[1].Taxi;
+  rows[2].textContent = data[1].Bus;
+  rows[3].textContent = data[1].Metro;
+  rows[4].textContent = data[1].All;
+  rows[5].textContent = data[1]["2x"];
+
+  return trElement;
+};
+
+const renderPlayerInfo = (rolesObject, ticketsObject) => {
+  const table = cloneTemplate("#roles-table");
   const popup = document.querySelector("#pop-up");
 
-  const table = cloneTemplate("#roles-table");
-  const tbody = table.querySelector("tbody");
+  const popup1 = table.querySelector(".pop-up-1");
+  const popup2 = table.querySelector(".pop-up-2");
+
+  const tbody1 = popup1.querySelector("tbody");
+  const tbody2 = popup2.querySelector("tbody");
 
   const roles = Object.entries(rolesObject);
-  const tableRows = roles.map(makeRow);
+  const tickets = Object.entries(ticketsObject);
+  const tableRoleRows = roles.map(makeRoleRow);
+  const tableTicketRows = tickets.map(makeTicketRow);
 
-  tbody.append(...tableRows);
+  tbody1.append(...tableRoleRows);
+  tbody2.append(...tableTicketRows);
   popup.appendChild(table);
+  console.log(popup, table, tbody1, tbody2);
 
   setTimeout(() => {
     popup.remove();
   }, 10000);
 };
 
-const renderTickets = (tickets) => {
-  console.log(tickets);
-};
-
 const fetchRoles = () => fetch("/game/info").then((res) => res.json());
 
 const main = async () => {
   const { roles, tickets } = await fetchRoles();
-  renderRoles(roles);
-  renderTickets(tickets);
+  renderPlayerInfo(roles, tickets);
 };
 
 globalThis.onload = main;
