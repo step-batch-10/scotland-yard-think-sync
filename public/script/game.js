371155548d9a3201e3bb2b83314fb78b1ee90fa9
PruthviLahare <142437821+PruthviLahare@users.noreply.github.com>
2025-04-18 17:30:26 +0530

    [#9] | Pruthvi/Akash | constantly checking game state to get live information - fixed the problem of disappering the game state table even before toggling the button - file affected :- game.html, game.js

--- public/script/game.js
+++ public/script/game.js
@@ -1,5 +1,3 @@
-import { combineObjects } from "./game_utils.js";
-
 const fetchState = () => fetch("/game/state").then((res) => res.json());
 const fetchRoles = () => fetch("/game/info").then((res) => res.json());
 
@@ -19,8 +17,7 @@ const makeRoleRow = (data) => {
   return trElement;
 };
 
-const playerStats = ([role, playerName, tickets, station]) => {
-  const trElement = cloneTemplate("#ticket-row");
+const playerStats = (trElement, [role, playerName, tickets, station]) => {
   const cells = trElement.querySelectorAll("td");
 
   cells[0].textContent = role;
@@ -35,17 +32,29 @@ const playerStats = ([role, playerName, tickets, station]) => {
   return trElement;
 };
 
+const combine3Object = (obj1, obj2, obj3) => {
+  const result = [];
+
+  for (const key in obj1) {
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+    const value3 = obj3[key];
+
+    result.push([key, value1, value2, value3]);
+  }
+
+  return result;
+};
+
 const renderPlayerTickets = (tickets, roles, positions) => {
-  const statsTable = cloneTemplate("#stats");
-  const tbody = statsTable.querySelector("tbody");
-  const tableContainer = document.querySelector("#ticket-table");
+  const playerStatTable = document.querySelector(".player-stats");
+  const tbody = playerStatTable.querySelector("tbody");
+  const stats = combine3Object(roles, tickets, positions);
+  const rows = tbody.children;
 
-  const stats = combineObjects(roles, tickets, positions);
-
-  const tableTicketRows = stats.map(playerStats);
-
-  tbody.append(...tableTicketRows);
-  tableContainer.replaceChildren(statsTable);
+  for (let index = 0; index < stats.length; index++) {
+    playerStats(rows[index], stats[index]);
+  }
 };
 
 const renderPlayer = (rolesObject) => {
@@ -61,46 +70,15 @@ const renderPlayer = (rolesObject) => {
 
   setTimeout(() => {
     popup.remove();
-  }, 1000);
-};
-
-const printStationDetails = (e) => {
-  alert(e.target.id);
-  console.log(e.target.getAttribute("x"));
-};
-
-const movePlayer = () => {
-  const stations = document.querySelectorAll("tspan");
-  stations.forEach((station) => {
-    station.addEventListener("click", printStationDetails);
-  });
-};
-
-const renderPawns = (roles, tickets, positions) => {
-  const stats = combineObjects(roles, tickets, positions);
-  const svg = document.querySelector("body");
-  const player = stats[1];
-  const position = player.at(-1);
-  const rect = document.querySelector(`#station-${position}`);
-
-  const pawn = cloneTemplate("#move-pawn");
-  pawn.style.border = "1px solid black";
-  pawn.style.position = "absolute";
-  pawn.style.left = `${rect.getAttribute("x")}px`;
-  pawn.style.top = `${rect.getAttribute("y")}px`;
-
-  svg.appendChild(pawn);
+  }, 10000);
 };
 
 const main = async () => {
   const { roles } = await fetchRoles();
-  renderPlayer(roles);
-
   const { tickets, positions } = await fetchState();
-  renderPlayerTickets(tickets, roles, positions);
 
-  renderPawns(roles, tickets, positions);
-  movePlayer();
+  renderPlayer(roles);
+  renderPlayerTickets(tickets, roles, positions);
 };
 
 globalThis.onload = main;
