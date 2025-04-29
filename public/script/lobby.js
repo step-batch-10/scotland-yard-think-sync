5121438023a4c5c0763b088352a4a513bf1bc252
akshay-011 <102289107+akshay-011@users.noreply.github.com>
2025-04-29 14:01:22 +0530

    [#0] | akshay/pruthvi | Restyles pages

--- public/script/lobby.js
+++ public/script/lobby.js
@@ -1,25 +1,40 @@
 import { redirectTo } from "./waiting.js";
 
-const hostGame = async () => {
-  const playerCount = document.querySelector("#playerCount");
-  const body = new FormData();
-  body.set("playerCount", playerCount.value);
-
-  const response = await fetch("/setup/create-room", { method: "POST", body });
-
-  if (response.ok) {
-    redirectTo("/html/waiting.html");
-  }
-};
-
 const joinGame = () => redirectTo("/html/join.html");
+
+const renderPopUp = () => {
+  document.querySelector("#popup-container").style.display = "block";
+}
+
+const fetchHost = async (body) => {
+  const { success } = await fetch('/setup/create-room', {
+    method: "POST",
+    body
+  }).then(res => res.json())
+
+  return success
+}
 
 const main = () => {
   const host = document.getElementById("host");
   const join = document.getElementById("join");
+  const buttons = document.querySelectorAll(".player-selection");
+  
+  buttons.forEach((button) => {
+    button.addEventListener("click", async (event) => {
+      const id = event.target.id;
+      const body = new FormData();
+      body.set("playerCount", id);
 
-  host.addEventListener("click", hostGame);
+      const result = await fetchHost(body);
+
+      if (result) redirectTo("/html/waiting.html");
+    })
+  })
+
+  host.addEventListener("click", renderPopUp);
   join.addEventListener("click", joinGame);
+  
 };
 
 globalThis.onload = main;
