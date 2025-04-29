0b89d0a23e788e951b57ca297dd44637ed246dd9
akshay-011 <102289107+akshay-011@users.noreply.github.com>
2025-04-29 14:03:19 +0530

    [#0] | Akshay\Pruthvi | Adds styles to pages

--- public/script/lobby.js
+++ public/script/lobby.js
@@ -4,22 +4,22 @@ const joinGame = () => redirectTo("/html/join.html");
 
 const renderPopUp = () => {
   document.querySelector("#popup-container").style.display = "block";
-}
+};
 
 const fetchHost = async (body) => {
-  const { success } = await fetch('/setup/create-room', {
+  const { success } = await fetch("/setup/create-room", {
     method: "POST",
-    body
-  }).then(res => res.json())
+    body,
+  }).then((res) => res.json());
 
-  return success
-}
+  return success;
+};
 
 const main = () => {
   const host = document.getElementById("host");
   const join = document.getElementById("join");
   const buttons = document.querySelectorAll(".player-selection");
-  
+
   buttons.forEach((button) => {
     button.addEventListener("click", async (event) => {
       const id = event.target.id;
@@ -29,12 +29,11 @@ const main = () => {
       const result = await fetchHost(body);
 
       if (result) redirectTo("/html/waiting.html");
-    })
-  })
+    });
+  });
 
   host.addEventListener("click", renderPopUp);
   join.addEventListener("click", joinGame);
-  
 };
 
 globalThis.onload = main;
